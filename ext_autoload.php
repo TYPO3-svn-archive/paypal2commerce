6961d752adb78f46b2fabd1079708e6f8701622d
tox234 <tox234@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-03 16:05:18 +0000

    Fixed provider autoload class
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/paypal2commerce/branches/enet_1.0@37659 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -2,7 +2,7 @@
 $extensionPath = t3lib_extMgm::extPath('paypal2commerce');
 
 return array(
-	'tx_paypal2commerce_paypal_provider' => $extensionPath . 'classes/class.tx_paypal2commerce_paypal_provider.php',
+	'tx_paypal2commerce_provider_paypal' => $extensionPath . 'classes/class.tx_paypal2commerce_provider_paypal.php',
 	'tx_paypal2commerce_payment_paypal_express' => $extensionPath . 'classes/class.tx_paypal2commerce_payment_paypal_express.php',
 	'tx_paypal2commerce_payment_exception' => $extensionPath . 'classes/class.tx_paypal2commerce_payment_exception.php',
 );
